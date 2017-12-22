5fdb8aaf65a0a0c0f8447734abbe7e4bf166c964
Gonçalo Pereira <goncalo.pereira@MPT01016.local>
2017-12-22 16:35:13 +0000

    Visuals

--- Assets/BlackOps/BlackOps2D/Scripts/CameraScript.cs
+++ Assets/BlackOps/BlackOps2D/Scripts/CameraScript.cs
@@ -17,29 +17,50 @@ public class CameraScript : NetworkBehaviour {
 	public Vector3 minCameraPos;
 	public Vector3 maxCameraPos;
 
-	// Use this for initialization
-	void Start () 
-	{
+	public Transform playerTransform;
+	public int depth = -20;
 
-	}
 	
 	// Update is called once per frame
-	void FixedUpdate () 
+/*	void FixedUpdate () 
 	{
 
-		player = GameObject.Find ("Player(Clone)");
+
 		if (player != null) {
-			float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
-			float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
-			transform.position = new Vector3 (posX, posY, transform.position.z);
+				float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+				float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
-			if (bounds) {
-				transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x),
-					Mathf.Clamp (transform.position.y, minCameraPos.y, maxCameraPos.y),
-					Mathf.Clamp (transform.position.z, minCameraPos.z, maxCameraPos.z));
+				transform.position = new Vector3 (posX, posY, transform.position.z);
+
+				if (bounds) {
+					transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x),
+						Mathf.Clamp (transform.position.y, minCameraPos.y, maxCameraPos.y),
+						Mathf.Clamp (transform.position.z, minCameraPos.z, maxCameraPos.z));
+				}
 			}
-		}
+	}
 
+	public void OnStartClient()
+	{
+		player = GameObject.Find ("Player(Clone)");
+	}*/
+
+	void Update()
+	{
+		if(playerTransform != null)
+		{
+				transform.position = playerTransform.position + new Vector3(0,10,depth);
+				if (bounds) {
+					transform.position = new Vector3 (Mathf.Clamp (transform.position.x, minCameraPos.x, maxCameraPos.x),
+						Mathf.Clamp (transform.position.y, minCameraPos.y, maxCameraPos.y),
+						Mathf.Clamp (transform.position.z, minCameraPos.z, maxCameraPos.z));
+				}
+		}
+	}
+
+	public void setTarget(Transform target)
+	{
+		playerTransform = target;
 	}
 }
