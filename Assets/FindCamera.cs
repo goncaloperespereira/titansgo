483b1e60baca81d9ef738355915c56aeda598e78
Gonçalo Pereira <goncalo.pereira@MPT01016.local>
2018-01-15 09:45:41 +0000

    Camera Fixed

---
+++ Assets/FindCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FindCamera : NetworkBehaviour 
+{
+
+	public GameObject MainCamera;
+	// Use this for initialization
+	void Start () 
+	{
+		MainCamera = GameObject.Find ("Main Camera");
+		if (isLocalPlayer)
+		{
+			MainCamera.transform.parent = this.transform;
+			MainCamera.transform.localPosition = new Vector3 (0, 0, -10);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
